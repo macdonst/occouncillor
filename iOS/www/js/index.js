01a0f5cebefd9fe881f617f62b4c67c4de435090
Simon MacDonald <simon.macdonald@gmail.com>
2013-02-27 22:44:26 -0500

    Issue #11: Preliminary work for Need a page that shows list of Elected Officials

--- iOS/www/js/index.js
+++ iOS/www/js/index.js
@@ -19,6 +19,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
+        console.log("init");
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -26,6 +27,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
+        console.log("bind");
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -33,17 +35,55 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        console.log("we got a deviceready");
         app.receivedEvent('deviceready');
+        councillors.loadCouncillors();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    }
+};
+
+var councillors = {
+    items: [],
+    loadCouncillors: function() {
+        var that = this;
+        var request = new XMLHttpRequest();
+        request.open("GET", "file:///android_asset/www/data/elected_officials.json", true);
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                if (request.status == 200 || request.status == 0) {
+                    //console.log(JSON.stringify(this));
+                    that.items = JSON.parse(request.responseText);
+                    that.listCouncillors();
+                }
+            }
+        }
+        request.send();
+    },
+    listCouncillors: function() {
+        var list = document.createElement("ul");
+        var main = document.getElementById("main");
+        for(var i = 0; i < this.items.length; i++) {
+            var councillor = this.items[i];
+            console.log(JSON.stringify(councillor));
+            var item = document.createElement("li");
+            //console.log(JSON.stringify(this.items[i]));
+            item.setAttribute("id", councillor["District ID"]);
+            //item.appendChild(document.createTextNode("id" + i));
+            item.appendChild(document.createTextNode(
+                councillor["First name"] + " " + councillor["Last name"] +
+                " - " + councillor["District name"]));
+            list.appendChild(item);
+        }
+        main.appendChild(list);
     }
 };
