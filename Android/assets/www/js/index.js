81274cfa8169d212a15b9ee8703ef3b3b6b76808
Simon MacDonald <simon.macdonald@gmail.com>
2013-04-04 23:35:30 -0400

    Polishing...

--- Android/assets/www/js/index.js
+++ Android/assets/www/js/index.js
@@ -77,26 +77,41 @@ var councillors = {
         var main = document.getElementById("main");
         for(var i = 0; i < this.items.length; i++) {
             var councillor = this.items[i];
-            //console.log(JSON.stringify(councillor));
+            //console.log(JSON.stringify(councillor));        
             var item = document.createElement("li");
             item.setAttribute("id", councillor["District ID"]);
-            item.appendChild(document.createTextNode(
-                councillor["First name"] + " " + councillor["Last name"] +
-                " - " + councillor["District name"]));
+            var cpic = document.createElement("img");
+            cpic.setAttribute("src", councillor["Photo URL"]);
+            cpic.setAttribute("class", "list-icon");
+            item.appendChild(cpic);
+            var line1 = document.createElement("p");
+            line1.setAttribute("class", "line1");
+            line1.appendChild(document.createTextNode(councillor["First name"] + " " + councillor["Last name"]))
+            var line2 = document.createElement("p");
+            line2.setAttribute("class", "line2");
+            line2.appendChild(document.createTextNode(councillor["District name"]))
+            item.appendChild(line1);
+            item.appendChild(line2);
             list.appendChild(item);
             document.body.appendChild(this.createPanel(councillor, text));
         }
         main.appendChild(list);
     },    
     showCouncillor: function(evt) {
-        console.log("I got clicked:" + evt.srcElement.id);
-        var panel = document.getElementById("panel"+evt.srcElement.id);
+        var srcElement = evt.srcElement;
+        while(srcElement.tagName != "LI") {
+            srcElement = srcElement.parentNode;
+        }
+        var panel = document.getElementById("panel"+srcElement.id);
         panel.setAttribute("style", "display: block");
         var main = document.getElementById("main");
         main.setAttribute("style", "display: none");
-        currentPanel = "panel"+evt.srcElement.id;
-        var that = this;
+        currentPanel = "panel"+srcElement.id;
+        var that = this;        
         document.addEventListener("backbutton", councillors.showMain, false);
+        
+        // hide that button 
+        document.getElementById("whereami").setAttribute("style", "display: none");
     }, 
     showMain: function() {
         console.log("did we get the back button event");
@@ -107,6 +122,9 @@ var councillors = {
         currentPanel = "main";
         var that=this;
         document.removeEventListener("backbutton", councillors.showMain, false);
+
+        // show that button 
+        document.getElementById("whereami").setAttribute("style", "display: block");
     },
     createPanel: function(councillor, text) {
         var panel = document.createElement("div");
